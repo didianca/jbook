3499b80187d00a33e2731bc62dac95d14bc20a1d
didineels <didi@neels.io>
2023-12-11 07:14:06 -0500

    add all case state updates through redux in the reducer using immer for seamless returns

--- src/state/reducers/cells.reducer.ts
+++ src/state/reducers/cells.reducer.ts
@@ -1,6 +1,8 @@
 import { Action } from '../actions';
 import { Cell } from '../cell';
 import { ActionType } from '../action-types';
+import { produce } from 'immer';
+import { uuid } from 'uuidv4';
 
 interface CellsState {
   loading: boolean;
@@ -18,19 +20,47 @@ const initialState: CellsState = {
   data: {},
 };
 
-const reducer = (state: CellsState, action: Action): CellsState => {
-  switch (action.type) {
-    case ActionType.UPDATE_CELL:
-      return state;
-    case ActionType.INSERT_CELL_BEFORE:
-      return state;
-    case ActionType.MOVE_CELL:
-      return state;
-    case ActionType.DELETE_CELL:
-      return state;
-    default:
-      return state;
+const reducer = produce(
+  (state: CellsState, action: Action): CellsState | void => {
+    switch (action.type) {
+      case ActionType.UPDATE_CELL:
+        const { id, content } = action.payload;
+        state.data[id].content = content;
+        return state;
+      case ActionType.INSERT_CELL_BEFORE:
+        const cell: Cell = {
+          id: uuid(),
+          content: '',
+          type: action.payload.type,
+        };
+        state.data[cell.id] = cell;
+        const foundIndex = state.order.findIndex(
+          (id) => id === action.payload.id
+        );
+        if (foundIndex < 0) {
+          state.order.push(cell.id);
+        } else {
+          state.order.splice(foundIndex, 0, cell.id);
+        }
+        return state;
+      case ActionType.MOVE_CELL:
+        const { direction } = action.payload;
+        const index = state.order.findIndex((id) => id === action.payload.id);
+        const targetIndex = direction === 'up' ? index - 1 : index + 1;
+        if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+          return state;
+        }
+        state.order[index] = state.order[targetIndex];
+        state.order[targetIndex] = action.payload.id;
+        return state;
+      case ActionType.DELETE_CELL:
+        delete state.data[action.payload];
+        state.order = state.order.filter((id) => id !== action.payload);
+        return state;
+      default:
+        return state;
+    }
   }
-};
+);
 
 export default reducer;
