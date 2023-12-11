7ed37905dc39f7baddac530bff18546525101e67
didineels <didi@neels.io>
2023-12-11 08:40:53 -0500

    cleanup store.ts

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,17 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducer from './reducers';
-import { ActionType } from './action-types';
 
 export const store = configureStore({
   reducer,
 });
-
-store.dispatch({
-  type: ActionType.INSERT_CELL_BEFORE,
-  payload: {
-    id: null,
-    type: 'code',
-  },
-});
-
-console.log(store.getState());
